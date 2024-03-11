da3bdf59542a86efe6dad5df4450935b55493cde
tabordapc56 <isabelataborda2005@gmail.com>
2024-03-10 23:57:00 -0500

    express js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("perros")
+const express = require("express")
+
+const app = express()
+
+app.get("/",(req,res) => {
+    res.send("hola")
+})
+
+app.listen(8080,() => {
+    console.log("servidor funcionando")
+})
